39752f67a12ecb2a5b1e8d3b8d125fb1e5cbb38c
Yarden Yosefzon <yardenyo8@gmail.com>
2023-07-23 00:13:58 +0300

    add events

---
+++ src/pages/logInTo/logInToCreateEvents.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { signIn } from 'next-auth/react'
+import { useRouter } from 'next/router'
+
+function LogInToBuyTickets(){
+
+  return (
+    <>
+      <div>!רוצה ליצור אירועים? התחבר בשנייה עם חשבון גוגל</div>
+      <button onClick={()=>signIn('google', { callbackUrl: `http://localhost:3000` }) }>התחבר עם חשבון גוגל</button>
+    </>
+
+  )
+}
+
+export default LogInToBuyTickets
